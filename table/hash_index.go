f8cbd33e7e387c733d441f360fb31fcef49f6f86
Zejun Li <me@zejun.li>
2018-11-07 19:47:54 +0800

    rename restart -> blockIdx

--- table/hash_index.go
+++ table/hash_index.go
@@ -20,10 +20,10 @@ type hashIndexBuilder struct {
 type indexEntry struct {
 	hash uint32
 
-	// restart is the index of block (aka restart) which contains this key.
-	restart uint16
+	// blockIdx is the index of block which contains this key.
+	blockIdx uint16
 
-	// offset is the index of this key in block (aka restart).
+	// offset is the index of this key in block.
 	offset uint8
 }
 
@@ -33,14 +33,14 @@ func newHashIndexBuilder(hashUtilRatio float32) hashIndexBuilder {
 	}
 }
 
-func (b *hashIndexBuilder) addKey(key []byte, restart uint32, offset uint8) {
-	if restart > maxRestart {
+func (b *hashIndexBuilder) addKey(key []byte, blkIdx uint32, offset uint8) {
+	if blkIdx > maxRestart {
 		b.invalid = true
 		b.entries = nil
 		return
 	}
 	h := farm.Fingerprint32(key)
-	b.entries = append(b.entries, indexEntry{h, uint16(restart), offset})
+	b.entries = append(b.entries, indexEntry{h, uint16(blkIdx), offset})
 }
 
 func (b *hashIndexBuilder) finish(buf []byte) []byte {
@@ -59,11 +59,11 @@ func (b *hashIndexBuilder) finish(buf []byte) []byte {
 	for _, h := range b.entries {
 		idx := h.hash % numBuckets
 		bucket := buckets[idx*3 : (idx+1)*3]
-		restart := binary.LittleEndian.Uint16(bucket[:2])
-		if restart == resultNoEntry {
-			binary.LittleEndian.PutUint16(bucket[:2], h.restart)
+		blkIdx := binary.LittleEndian.Uint16(bucket[:2])
+		if blkIdx == resultNoEntry {
+			binary.LittleEndian.PutUint16(bucket[:2], h.blockIdx)
 			bucket[2] = h.offset
-		} else if restart != h.restart {
+		} else if blkIdx != h.blockIdx {
 			binary.LittleEndian.PutUint16(bucket[:2], resultFallback)
 		}
 	}
@@ -89,6 +89,6 @@ func (i *hashIndex) lookup(key []byte) (uint32, uint8) {
 	h := farm.Fingerprint32(key)
 	idx := int(h) % i.numBuckets
 	buf := i.buckets[idx*3:]
-	restart := binary.LittleEndian.Uint16(buf)
-	return uint32(restart), uint8(buf[2])
+	blkIdx := binary.LittleEndian.Uint16(buf)
+	return uint32(blkIdx), uint8(buf[2])
 }
